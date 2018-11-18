d4665e2a0fd1dc1c7e70e5eabaf21c2d6327d540
Ram Prakash <ramprakash@Rams-MacBook-Pro.local>
2018-11-17 19:44:10 -0500

    Added annotations

--- org-insert-url.el
+++ org-insert-url.el
@@ -1,4 +1,28 @@
+;;; org-insert-url.el --- A lightweight package to insert URLs as bookmarks in org
+     
+;; Copyright (C) 2018 Ram Prakash Arivu Chelvan 
+
+;; Author: Ram Prakash Arivu Chelvan <ramprakash.94@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((org "9.0"))
+;; Keywords: url, bookmark, org
+;; URL: http://example.com/jrhacker/superfrobnicate
+
+;;; Commentary:
+
+;; org-insert-url.el is a lightweight package created to help you insert
+;; external URLs into your org document with the webpage title. The package 
+;; extracts the title from the webpage and inserts it into your org document 
+;; with a hyperlink to the URL.
+
+;; It also offers a function to directly take the URL from your clipboard
+;; and insert it as new bullet in your org document.
+
+;; Full documentation is available as an Info manual.
+
+;;; Code:
 (require 'org)
+
 (defun extract-text-in-tag (url start-tag end-tag)
   (interactive "MURL: ")
   (switch-to-buffer "*temp-bookmark*")
